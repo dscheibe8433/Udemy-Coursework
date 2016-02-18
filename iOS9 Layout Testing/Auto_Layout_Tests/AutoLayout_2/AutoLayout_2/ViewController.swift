611161986682fc45578320ed1ce39cfb59b053d2
dscheibe8433 <dscheibe@g.ucla.edu>
2016-02-17 19:12:46 -0800

    Auto Layout 2

---
+++ iOS9 Layout Testing/Auto_Layout_Tests/AutoLayout_2/AutoLayout_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout_2
+//
+//  Created by David Scheibe on 2/17/16.
+//  Copyright © 2016 David Scheibe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
